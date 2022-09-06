c7b4de7c61962a83b8ffc701dd42732814063f8b
Stevens <sdemervil@gmail.com>
2022-09-06 11:42:16 +0200

    feat: add email preview and tree view container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import type {NextPage} from 'next'
 
 const Home: NextPage = () => {
-    return (
-        <main>
+    const treeViewStyle = {
+        height: '100vh',
+        backgroundColor: '#DCDCDC',
+        flex: '1 0 auto',
+        borderLeft: '6px solid #CCC'
+    }
 
-        </main>
+    const emailPreviewStyle = {
+        height: '100vh',
+        backgroundColor: '#F2F2F2',
+        flex: '2.5 1 auto'
+    }
+
+    return (
+        <>
+            <div className="email-preview" style={emailPreviewStyle}></div>
+            <div className="tree-view" style={treeViewStyle}></div>
+        </>
     )
 }
 
